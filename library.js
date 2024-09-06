88c4cc9eb9cc6bbd045b26807709b54952e19362
ZakhaevK <zkupke@gmail.com>
2024-09-06 16:00:35 +1000

    Add JS for displaying game objects in myLibrary - CSS Style modified slightly

--- library.js
+++ library.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 
+const libContainer = document.getElementById('library');
+
 function game(title, dev, length, played) {
   this.title = title;
   this.dev = dev;
@@ -19,7 +21,31 @@ function addGameToLibrary(game) {
 }
 
 function displayGames() {
-  for (game in myLibrary) {
-    return game.info();
+  for (game of myLibrary) {
+    const newGame = document.createElement('div');
+    newGame.className = "game";
+
+    const titleH2 = document.createElement('h2');
+    titleH2.textContent = game.title;
+
+    const devPara = document.createElement('p');
+    devPara.textContent = `Developer: ${game.dev}`;
+
+    const lengthPara = document.createElement('p');
+    lengthPara.textContent = `Length: ${game.length} hours`;
+
+    const playedPara = document.createElement('p');
+    if (game.played) {
+      playedPara.textContent = 'Played: Yes';
+    } else {
+      playedPara.textContent = 'Played: No';
+    }
+
+    // Add to document
+    newGame.appendChild(titleH2);
+    newGame.appendChild(devPara);
+    newGame.appendChild(lengthPara);
+    newGame.appendChild(playedPara);
+    libContainer.appendChild(newGame);
   }
 }
